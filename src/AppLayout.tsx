3d2913289ede877504202dce2a00d6540b7f5d3b
isydun <illia.sydun@vestiacom.com>
2021-06-27 01:44:39 +0200

    Improve AppLayout, add ErrorBoundary and use LanguageSelector

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -1,12 +1,36 @@
 import React, { ReactNode } from "react";
 import style from "./AppLayout.module.scss";
+import LanguageSelector from "./components/language_selector/LanguageSelector";
+import PageRefresh from "./components/page_refresh/PageRefresh";
 
 interface IProps {
   children: ReactNode;
 }
 
-const AppLayout = ({ children }: IProps) => {
-  return <div className={style.container}>{children}</div>;
-};
+interface IState {
+  hasError: boolean;
+}
+
+class AppLayout extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch() {
+    this.setState({
+      hasError: true,
+    });
+  }
+
+  render() {
+    return (
+      <div className={style.container}>
+        <LanguageSelector />
+        {this.state.hasError ? <PageRefresh /> : this.props.children}
+      </div>
+    );
+  }
+}
 
 export default AppLayout;
