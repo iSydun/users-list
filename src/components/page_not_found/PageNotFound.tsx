0d41cfe280bb476c00cfa72e48d85dc8e43602d7
isydun <illia.sydun@vestiacom.com>
2021-06-26 20:22:23 +0200

    Add "PageNotFound" component

---
+++ src/components/page_not_found/PageNotFound.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom";
+import style from "./PageNotFound.module.scss";
+import CustomButton from "../custom_button/CustomButton";
+
+const PageNotFound = () => {
+  const { t } = useTranslation("Common");
+  const history = useHistory();
+
+  const goToUsersList = () => {
+    history.push("/users");
+  };
+
+  return (
+    <div className={style.container}>
+      <div className={style.title}>{t("pageNotFoundTitle")}</div>
+      <div className={style.button}>
+        <CustomButton title={t("goBack")} onClick={goToUsersList} />
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
