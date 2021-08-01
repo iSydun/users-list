f96b5a02927a83caaa2a59b7dde91c58493e00b5
isydun <illia.sydun@vestiacom.com>
2021-08-01 20:16:23 +0200

    tiny

--- src/test-utils/index.tsx
+++ src/test-utils/index.tsx
@@ -4,14 +4,14 @@ import store from "../reducers";
 import { Provider } from "react-redux";
 import "@testing-library/jest-dom";
 
-const AllTheProviders: FC = ({ children }) => {
+const StoreProvider: FC = ({ children }) => {
   return <Provider store={store}>{children}</Provider>;
 };
 
 const customRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, "queries">
-) => render(ui, { wrapper: AllTheProviders, ...options });
+) => render(ui, { wrapper: StoreProvider, ...options });
 
 export * from "@testing-library/react";
 
